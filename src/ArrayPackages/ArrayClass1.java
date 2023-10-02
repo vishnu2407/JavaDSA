b545a555c0a2113b4cd6de353c9b03bd562f098c
vishnu2407 <avatapallivishnu@gamil.com>
2023-10-03 00:26:21 +0530

    Initial commit

---
+++ src/ArrayPackages/ArrayClass1.java
@@ -0,0 +1,27 @@
+package ArrayPackages;
+import java.lang.*;
+
+public class ArrayClass1
+{
+    int[] arr1 = {1, 2, 3, 4, 5};
+    int[] arr2 = {6, 7, 8, 9, 10};
+
+    public static void main(String[] args){
+        ArrayClass1 obj = new ArrayClass1();
+
+        System.out.println("arr1:");
+        for (int i = 0; i < obj.arr1.length; i++){
+            System.out.print(obj.arr1[i] + " ");
+        }
+        System.out.println();
+
+        System.out.println("arr2:");
+        for (int i = 0; i < obj.arr1.length; i++){
+            System.out.print(obj.arr1[i] + " ");
+        }
+        System.out.println();
+
+//        System.out.println("arr1: " + Arrays.toString(obj.arr1));
+//        System.out.println("arr2: " + Arrays.toString(obj.arr2));
+    }
+}
