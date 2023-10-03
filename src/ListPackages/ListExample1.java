7b2b09f4b3a2c24469b0b4127ad1be02dcbe33b4
vishnu2407 <avatapallivishnu@gamil.com>
2023-10-04 02:34:52 +0530

    List updated code

--- src/ListPackages/ListExample1.java
+++ src/ListPackages/ListExample1.java
@@ -6,7 +6,6 @@ import java.util.List;
 public class ListExample1 {
     public static void main(String[] args) {
         List<String> list = new ArrayList<>();
-
         list.add("Vishnu");
         list.add("Shiva");
         System.out.println(list);
