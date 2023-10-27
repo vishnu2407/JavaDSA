704f25a2fe9b5907f181df33360d4963a2f2e793
vishnu2407 <vishnu@gmail.com>
2023-10-28 01:16:46 +0530

    Hello world

--- src/PackageInheritance/ClassA.java
+++ src/PackageInheritance/ClassA.java
@@ -5,6 +5,7 @@ public abstract class ClassA {
     public void Animal(String name, String action) {
         System.out.println(name + " is " + action);
     }
+    public void Human(String name, String action) {System.out.println(name + " is " + action); }
 
     public abstract void Human(String name);
 
--- src/PackageInheritance/ClassC.java
+++ src/PackageInheritance/ClassC.java
@@ -3,19 +3,23 @@ package PackageInheritance;
 public class ClassC extends ClassA implements ClassB {
     public static void main(String[] args) {
         ClassC obj = new ClassC();
-        obj.Animal("Dog", "Run");
-        obj.Animal("Lion", "Eat");
-        obj.Animal("Cat", "Sleep");
-        obj.Animal("Fish", "Swim");
-        obj.Human("John", "Run");
-        obj.Human("Alice", "Eat");
-        obj.Human("Bob", "Sleep");
-        obj.Human("Eve", "Work");
-        obj.Human("Charlie", "Play");
-        obj.Human("Samantha", "Swim");
-        obj.Human("Michael", "Dance");
-        obj.Human("Emma", "Sing");
-        obj.Human("David", "Talk");
+         {
+            obj.Animal("Dog", "Run");
+            obj.Animal("Lion", "Eat");
+            obj.Animal("Cat", "Sleep");
+            obj.Animal("Fish", "Swim");
+        }
+        {
+            obj.Human("John", "Run");
+            obj.Human("Alice", "Eat");
+            obj.Human("Bob", "Sleep");
+            obj.Human("Eve", "Work");
+            obj.Human("Charlie", "Play");
+            obj.Human("Samantha", "Swim");
+            obj.Human("Michael", "Dance");
+            obj.Human("Emma", "Sing");
+            obj.Human("David", "Talk");
+        }
     }
 
     @Override
