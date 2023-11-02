55414ca43ad2a6ac68ddd477c9c34ad44e939aaa
Avatapalli Vishnuvardhan Rao <avatapallivishnu@gmail.com>
2023-11-02 22:06:09 +0530

    loop statements

--- src/Loops/loop8.java
+++ src/Loops/loop8.java
@@ -1,8 +1,8 @@
 package Loops;
-import java.util.Scanner;
+//import java.util.Scanner;
 public class loop8 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        //Scanner sc = new Scanner(System.in);
         for(int i =0; i<=10; i++){
             if(i==4){
                 System.out.println("Breaking out of the loop");
