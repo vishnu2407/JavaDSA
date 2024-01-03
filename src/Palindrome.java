c5922ea35b9c5810994fae24e7bb128c7bcec44f
Avatapalli Vishnuvardhan Rao <avatapallivishnu@gmail.com>
2024-01-04 02:33:55 +0530

    ..

---
+++ src/Palindrome.java
@@ -0,0 +1,27 @@
+public class Palindrome {
+    public static void main(String[] args) {
+
+        // Your code for the main method goes here, if needed
+        int number = 110;
+        boolean isPalindrome = true;
+        if (number < 0) {
+            isPalindrome = false;
+        } else {
+            int temp = number;
+            int reverse = 0;
+            while (temp > 0) {
+                int lastDigit = temp % 10;
+                reverse = reverse * 10 + lastDigit;
+                temp /= 10;
+            }
+            if (reverse != number) {
+                isPalindrome = false;
+            }
+        }
+        if (isPalindrome) {
+            System.out.println(number + " is a palindrome number.");
+        } else {
+            System.out.println(number + " is not a palindrome number.");
+        }
+    }
+}
