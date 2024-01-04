ec925d5394dfeaf24a9005491d4715270ba59ce8
Avatapalli Vishnuvardhan Rao <avatapallivishnu@gmail.com>
2024-01-04 23:49:54 +0530

    loops and conditions

---
+++ src/Loops/whileLoop1.java
@@ -0,0 +1,21 @@
+package Loops;
+
+public class whileLoop1 {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        //while loop in java
+        //Q: print numbers from 1 to 10 using while loop
+        /*syntax for while loop
+            while(condition)
+            {
+                //body of while loop
+            }
+        */
+        int i = 0;
+        while (i <= 10) {
+            System.out.print(i + " ");
+            i += 1; // i = i + 2.getRuntime().gc();
+            i++;
+        }
+    }
+}
