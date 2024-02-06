6914bfd1eb8aae88c8ae06f0d1a21147e511a4db
VVR RAO <Avatapallyvishnu@gmail.com>
2024-02-06 21:42:44 +0530

    Nested Switch case

--- src/Loops/countNumbers.java
+++ src/Loops/countNumbers.java
@@ -1,10 +1,8 @@
 package Loops;
 
-import java.util.Scanner;
-
 public class countNumbers {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        //Scanner sc = new Scanner(System.in);
         int n = 50000;
         int count = 0;
         while (n > 0) {
