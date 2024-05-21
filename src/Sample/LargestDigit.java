3117421bd7e384182a7b28fcc4c96019eb4adab1
Vishnu <avatapallivishnu@gmail.com>
2024-05-22 01:18:40 +0530

    Time Complexity

---
+++ src/Sample/LargestDigit.java
@@ -0,0 +1,28 @@
+package Sample;
+
+public class LargestDigit {
+
+    public static void main(String[] args) {
+        double now = System.currentTimeMillis();
+        LargestDigit obj = new LargestDigit();
+        System.out.println(obj.findLargestDigit(9, 99, 999, 9999, 99999));
+        System.out.println("Time taken: " + (System.currentTimeMillis() - now) + "ms");
+    }
+
+    private double findLargestDigit(double a, double b, double c, double d , double e) {
+        double max = a;
+        if (b > max) {
+            max = b;
+        }
+        if (c > max) {
+            max = c;
+        }
+        if (d > max) {
+            max = d;
+        }
+        if (e > max) {
+            max = e;
+        }
+        return max;
+    }
+}
