237e2d09a9a4ed2c8ddfdab0c930294d37d31bbc
Vishnu <avatapallivishnu@gmail.com>
2024-07-09 20:36:39 +0530

    array concepts zero at last

--- src/Array/missing_array.java
+++ src/Array/missing_array.java
@@ -2,13 +2,13 @@ package Array;
 
 public class missing_array {
     public static void main(String[] args) {
-        int[] arr1 = {1, 2, 3, 4, 5};
-        int[] arr2 = {1, 2, 3, 0, 5};
+        int[] arr = {1, 2, 3, 4, 5, 7, 9, 10, 8};
+        int n = arr.length + 1;
+        int sum = n * (n + 1) / 2;
 
-        for (int i = 0; i < arr1.length; i++) {
-            if (arr1[i] != arr2[i]) {
-                System.out.println(arr1[i]);
-            }
+        for (int i = 0; i < arr.length; i++) {
+            sum -= arr[i];
         }
+        System.out.println(sum);
     }
 }
