03e15e783ba201069a6569640429c388563a8f05
Vishnu <avatapallivishnu@gmail.com>
2024-10-04 00:04:16 +0530

    Worked on HashSet and HashMap in Java as well practicing the code in the IDE.

---
+++ src/Hash_Tables/HashSet/HashSet_Exp2.java
@@ -0,0 +1,26 @@
+package Hash_Tables.HashSet;
+
+import java.util.*;
+
+public class HashSet_Exp2 {
+    //Union of 2arrays using HashSet
+    public static int union(int[] arr1, int[] arr2) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i=0; i<arr1.length; i++){
+            set.add(arr1[i]);
+            for(int j=0; j<arr2.length; j++){
+                set.add(arr2[j]);
+            }
+            //return set.size();
+        }
+        return set.size();
+    }
+    public static void main(String[] args) {
+        int arr1[] = {7, 3, 9};
+        int arr2[] = {6, 3, 9, 2, 9, 4};
+
+        System.out.println("Union of 2 arrays: " + union(arr1, arr2));
+
+    }
+}
