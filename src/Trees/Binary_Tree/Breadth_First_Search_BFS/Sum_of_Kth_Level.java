3a2df69d1661064e72103f81ff2b92eb0b9e9eb0
Vishnu <avatapallivishnu@gmail.com>
2024-10-05 02:22:22 +0530

    Working on Binary Tree in Java as well practicing the code in the IDE.

--- src/Trees/Binary_Tree/Breadth_First_Search_BFS/Sum_of_Kth_Level.java
+++ src/Trees/Binary_Tree/Breadth_First_Search_BFS/Sum_of_Kth_Level.java
@@ -2,11 +2,12 @@ package Trees.Binary_Tree.Breadth_First_Search_BFS;
 
 import java.util.*;
 
-public class Levelorder_Traversal {
+public class Sum_of_Kth_Level {
     //Node class
     static class Node{
         int data;
-        Node right, left;
+        Node right;
+        Node left;
 
         Node(int data){
             this.data= data;
@@ -29,48 +30,50 @@ public class Levelorder_Traversal {
             return newNode;
         }
     }
-    //Levelorder Traversal
-    static void levelorderTraversal(Node root){
+
+    //Function to find sum of nodes in kth level
+    static void sumofKthLevel(Node root, int k){
         if(root == null){
             return;
         }
-
         Queue<Node> q = new LinkedList<>();
         q.add(root);
-        q.add(null);
+        int level = 1;
         while(!q.isEmpty()){
-            Node cuurNode = q.remove();
-            if(cuurNode == null){
-                System.out.println();
-                if(q.isEmpty()){
-                    break;
-                }else{
-                    q.add(null);
+            int size = q.size();
+            int sum = 0;
+            for(int i=0; i<size; i++){
+                Node temp = q.poll();
+                if(level == k){
+                    sum += temp.data;
                 }
-            }else{
-                System.out.print(cuurNode.data + " ");
-                if(cuurNode.left!= null){
-                    q.add(cuurNode.left);
+                if(temp.left != null){
+                    q.add(temp.left);
                 }
-                if (cuurNode.right!= null){
-                    q.add(cuurNode.right);
+                if(temp.right != null){
+                    q.add(temp.right);
                 }
             }
+            if(level == k){
+                System.out.println("Sum of nodes in level " + k + " is " + sum);
+                return;
+            }
+            level++;
         }
-
     }
+
     public static void main(String[] args) {
         //Build Tree Preorder sequence
         int nodes[] ={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
 
         BinaryTree tree = new BinaryTree();
-       Node root =tree.buildTree(nodes);
+        Node root =tree.buildTree(nodes);
 
         //Time taken to build the tree
         double now = System.currentTimeMillis();
         System.out.println("Time taken: " + (System.currentTimeMillis() - now) + "ms");
 
-       //Levelorder Traversal
-        levelorderTraversal(root);
+        //Sum of nodes in kth level
+        sumofKthLevel(root, 2);
     }
 }
